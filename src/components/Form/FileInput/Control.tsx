6c411cb6bd3c14509ce761b58f62b19426f4e2ce
Leandro Campos <camposweb@gmail.com>
2023-11-18 22:33:58 -0300

    context and Preview Image

--- src/components/Form/FileInput/Control.tsx
+++ src/components/Form/FileInput/Control.tsx
@@ -1,3 +1,30 @@
-export function Control() {
-  return <></>
+'use client'
+
+import { ChangeEvent, ComponentProps } from 'react'
+import { useFileInput } from './Root'
+
+type ControlProps = ComponentProps<'input'>
+
+export function Control(props: ControlProps) {
+  const { id, onFilesSelected } = useFileInput()
+
+  function handleFilesSelected(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files?.length) {
+      return
+    }
+
+    const files = Array.from(event.target.files)
+
+    onFilesSelected(files)
+  }
+
+  return (
+    <input
+      type="file"
+      id={id}
+      onChange={handleFilesSelected}
+      className="sr-only"
+      {...props}
+    />
+  )
 }
--- src/components/Form/FileInput/Root.tsx
+++ src/components/Form/FileInput/Root.tsx
@@ -1,3 +1,32 @@
-export function Root() {
-  return <></>
+'use client'
+
+import {
+  ComponentProps,
+  createContext,
+  useContext,
+  useId,
+  useState,
+} from 'react'
+
+type RootProps = ComponentProps<'div'>
+
+interface FileInputContextProps {
+  id: string
+  files: File[]
+  onFilesSelected: (files: File[]) => void
 }
+
+const FileInputContext = createContext({} as FileInputContextProps)
+
+export function Root(props: RootProps) {
+  const id = useId()
+  const [files, setFiles] = useState<File[]>([])
+
+  return (
+    <FileInputContext.Provider value={{ id, files, onFilesSelected: setFiles }}>
+      <div {...props} />
+    </FileInputContext.Provider>
+  )
+}
+
+export const useFileInput = () => useContext(FileInputContext)
