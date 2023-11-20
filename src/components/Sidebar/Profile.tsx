6b85cffb10dee0285ff09091d870131d6ce542b0
Leandro Campos <camposweb@gmail.com>
2023-11-19 23:35:29 -0300

    variant ghost

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,5 +1,6 @@
 import { LogOut } from 'lucide-react'
 import Image from 'next/image'
+import { Button } from '../Button'
 
 export function Profile() {
   return (
@@ -17,9 +18,9 @@ export function Profile() {
           camposweb@gmail.com
         </span>
       </div>
-      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
-        <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      <Button type="button" variant="ghost">
+        <LogOut className="ml-auto h-5 w-5 text-zinc-500" />
+      </Button>
     </div>
   )
 }
